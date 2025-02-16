123517fd8f03f458cd167efa1d04c45f7336a92b
kimquinones <147838528+kimquinones@users.noreply.github.com>
2025-02-16 01:09:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to navigate to the next page
+function next() {
+  window.location.href = "page2.html";
+}
+
+// Function to navigate back to the previous page
+function goBack() {
+  window.location.href = "index.html";
+}
